f338a7f74d54be919536bd1d462670aecc6624de
Jenn Viau <jviau@fuze.com>
2018-07-04 15:13:32 -0400

    updating outputs.tf

--- terraform/modules/gke/outputs.tf
+++ terraform/modules/gke/outputs.tf
@@ -5,3 +5,21 @@ output "cluster_name" {
 output "cluster_zone" {
   value = "${google_container_cluster.cluster.zone}"
 }
+output "client_certificate" {
+  value     = "${google_container_cluster.keycloak-ha.master_auth.0.client_certificate}"
+  sensitive = true
+}
+
+output "client_key" {
+  value     = "${google_container_cluster.keycloak-ha.master_auth.0.client_key}"
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = "${google_container_cluster.keycloak-ha.master_auth.0.cluster_ca_certificate}"
+  sensitive = true
+}
+
+output "host" {
+  value     = "${google_container_cluster.keycloak-ha.endpoint}"
+  sensitive = true
